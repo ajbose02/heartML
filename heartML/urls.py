835f6ae2dbecb728a34246768af71a4bbe0f196b
Arjit Bose <ajbose02@gmail.com>
2023-02-13 12:06:35 -0500

    first prototype

--- heartML/urls.py
+++ heartML/urls.py
@@ -1,4 +1,4 @@
-"""hack URL Configuration
+"""URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -23,7 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('survey', views.survey, name='survey'),
-    path('about/', views.about, name='about'),
     path('prediction/<int:a>/<int:b>/<int:c>/<int:d>/<int:e>/<int:f>/<int:g>/<int:h>/<int:i>/<int:j>/<int:k>/<int:l>/<int:m>/<int:n>/<int:o>/<int:p>/<int:q>/', views.prediction, name="prediction"),
+    path('prediction/', views.pred, name="pred"),
+    path('goals/', views.goals, name="goals"),
     path('users/', include('users.urls')),
 ]
--- heartML/views.py
+++ heartML/views.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from django.shortcuts import render, get_object_or_404
-from .models import Person
+from .models import Person, Goal
 
 from django.contrib.auth.decorators import login_required
 
@@ -15,16 +15,23 @@ def index(request):
 def survey(request): 
     return render(request, 'ind.html')
 
-
-def about(request): 
-    return render(request, 'About.html')
-
-# #create another view function for prediction without data in args
-# @login_required
-# def prediction(request) {
-
-# }
-
+@login_required
+def goals(request):
+    goals = Goal.objects.filter(owner=request.user)
+    context = {'goals': goals}
+    return render(request, 'goals.html', context)
+    
+@login_required
+def pred(request): 
+    # people = Person.objects.filter(owner=request.user)
+    result = Person.objects.filter(owner=request.user)[:1].get().getResult()
+    split = str(result).split("|")
+    res = split[0]
+    subtext = split[1]
+    context = {'result': res, 'subtext': subtext}
+    
+    return render(request, 'index.html', context)
+   
 @login_required
 def prediction(request, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q):
     new_input = [[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q]]
@@ -55,16 +62,48 @@ def prediction(request, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q):
     result = ""
     subtext = ""
     new_output = model.predict(new_input)
-    person = Person(new_input)
-    print(person.getArray())
-    print(new_output)
-    # summarize input and output
     if(new_output == 1):
         result = "You are at risk for heart disease."
         subtext = "Although you are currently at risk, you can still reduce your chances by improving your health."
     else:
         result = "You are NOT at risk for heart disease."
         subtext = "Although you are currently not at risk, you can still reduce your chances by improving your health"
+    
+    res = result + "|" + subtext
+
+    person = Person(gender=a,bmi=b,smoking=c,alcohol=d,stroke=e,phealth=f,mhealth=g,diffwalking=h,age=i,race=j,diabetes=k,pact=l,ghealth=m,sleep=n,asthma=o,kidney=p,skin=q, result=res, owner=request.user)
+    person.save()
+
+    goals = []
+    descriptions = []
+
+    #generating goals
+    if(b>30):
+        goals.append("BMI")
+        d1 = "Your current BMI is " + str(b) + ". Ideally, you should target a BMI under 30 kg/m^2. In order to improve your BMI, exercise regularly and maintain a healthy diet."
+        descriptions.append(d1)
+    if(c==1):
+        goals.append("Smoking")
+        d2 = "Your current smoking habits contribute to your risk of develping heart disease. If you are addicted, get help by considering counseling."
+        descriptions.append(d2)
+    if(d==1): 
+        goals.append("Alcohol")
+        d3 = "Your current alcohol use contributes to your risk of developing heart disease. If you are addicted, get help by considering counseling."
+        descriptions.append(d3)
+    if(l==0):
+        goals.append("Physical Activity")
+        d4 = "Your lack of physical activity puts you at risk for heart disease. Aim for at least 150 min/week of aerobic activity/ Consider biking, swimming, and brisk walking."
+        descriptions.append(d4)
+    if(n<7):
+        goals.append("Sleep")
+        d5 = "You currently sleep " + str(n) + " hours per day. Lack of sleep increases your risk of heart disease, so target 7 hours of sleep every day. Prioritize your sleep by changing your daily routine."
+        descriptions.append(d5)
+    
+    for i in range(len(goals)):
+        goal = Goal(text = goals[i], description = descriptions[i], owner=request.user)
+        goal.save()
+        print("goal got saved")
+
     context = {
         'result': result,
         'subtext': subtext
